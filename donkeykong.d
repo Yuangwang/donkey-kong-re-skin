5b03f4ab528b148fa9c8b06726c122a522910b0c
Wei Wang <yuangwang2@gmail.com>
2017-04-25 01:29:58 -0500

    changed project name

---
+++ donkeykong.d
@@ -0,0 +1,7 @@
+.\donkeykong.o: DonkeyKong.c
+.\donkeykong.o: D:\wei\Documents\ee319k\keil\ARM\ARMCC\bin\..\include\stdint.h
+.\donkeykong.o: tm4c123gh6pm.h
+.\donkeykong.o: ST7735.h
+.\donkeykong.o: Random.h
+.\donkeykong.o: TExaS.h
+.\donkeykong.o: ADC.h
--- spaceinvaders.d
+++
@@ -1,7 +0,0 @@
-.\spaceinvaders.o: SpaceInvaders.c
-.\spaceinvaders.o: D:\wei\Documents\ee319k\keil\ARM\ARMCC\bin\..\include\stdint.h
-.\spaceinvaders.o: tm4c123gh6pm.h
-.\spaceinvaders.o: ST7735.h
-.\spaceinvaders.o: Random.h
-.\spaceinvaders.o: TExaS.h
-.\spaceinvaders.o: ADC.h
