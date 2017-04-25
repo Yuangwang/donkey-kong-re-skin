ceca0b98661ec14b5dba6d961380993b77e5b0dc
Creedy34 <Christopher.Reedy@utexas.edu>
2017-04-25 09:04:14 -0500

    fixed mario

--- sound.d
+++ sound.d
@@ -1,4 +1,4 @@
 .\sound.o: Sound.c
-.\sound.o: D:\wei\Documents\ee319k\keil\ARM\ARMCC\bin\..\include\stdint.h
+.\sound.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
 .\sound.o: Sound.h
 .\sound.o: DAC.h
